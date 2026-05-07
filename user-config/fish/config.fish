409cabfde57faac28e84741e563eebb0329dc1b7
bolens <bolens@duck.com>
2026-05-07 00:00:02 -0600

    Auto-sync: 2026-05-07 00:00:02 [7900X3D Workstation]

--- user-config/fish/config.fish
+++ user-config/fish/config.fish
@@ -28,6 +28,14 @@ set -gx VCPKG_ROOT "$HOME/.local/share/vcpkg"
 set -gx VCPKG_DISABLE_METRICS "1"
 set -gx NIX_CONFIG "experimental-features = nix-command flakes"
 
+# Normalize locale in Fish sessions to a generated UTF-8 locale.
+set -e -g LANGUAGE
+set -e -g LC_ALL
+for _locale_var in LC_CTYPE LC_NUMERIC LC_TIME LC_COLLATE LC_MONETARY LC_MESSAGES LC_PAPER LC_NAME LC_ADDRESS LC_TELEPHONE LC_MEASUREMENT LC_IDENTIFICATION
+    set -e -g $_locale_var
+end
+set -gx LANG "en_US.UTF-8"
+
 # Use micro for tools that honor EDITOR/VISUAL (e.g. sudoedit).
 set -gx EDITOR "micro"
 set -gx VISUAL "code-insiders --wait"
